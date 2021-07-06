2587a29e9167cda5ebf45b3deeb99ed69e0449e1
abir-oz <jahirul15-8163@diu.edu.bd>
2021-07-06 19:29:45 +0600

    done

--- src/Components/TablePage/TablePage.js
+++ src/Components/TablePage/TablePage.js
@@ -1,8 +1,10 @@
-import { MDBBtn, MDBDataTable } from 'mdbreact';
+import { MDBBtn, MDBDataTable, MDBInput } from 'mdbreact';
 import React, { useEffect, useState } from 'react';
 
 const TablePage = () => {
     const [users, setUsers] = useState([]);
+    const [gender, setGender] = useState('All');
+    // const [rowData, setowData] = useState([]);
     const url = 'https://jsonplaceholder.typicode.com/users';
 
     useEffect(() => {
@@ -15,8 +17,14 @@ const TablePage = () => {
         console.log('Clicked', e.target);
     };
 
-    const userList = users.map((user) => {
-        const heading5 = (
+    const handleSelect = (e) => {
+        const selectedGender = e.target.value;
+        setGender(selectedGender);
+    };
+    console.log(gender);
+
+    const userList = users.map((user, index) => {
+        const heading6 = (
             <MDBBtn onClick={handelClick} color="danger">
                 Delete
             </MDBBtn>
@@ -24,19 +32,30 @@ const TablePage = () => {
         const userData = {
             id: user.id,
             heading1: user.name,
-            heading2: user.email,
-            heading3: user.phone,
-            heading4: user.website,
-            heading5,
+            heading2: '',
+            heading3: user.email,
+            heading4: user.phone,
+            heading5: user.website,
+            heading6,
         };
+        if (index % 2 === 0) {
+            userData.heading2 = 'Male';
+        } else {
+            userData.heading2 = 'Female';
+        }
 
         return userData;
     });
+    let userFiltered;
+    if (gender) {
+        userFiltered = userList.filter((user) => user.heading2 === gender);
+        console.log(userFiltered);
+    }
 
     const data = {
         columns: [
             {
-                label: '#',
+                label: '',
                 field: 'id',
                 sort: 'asc',
             },
@@ -46,27 +65,32 @@ const TablePage = () => {
                 sort: 'asc',
             },
             {
-                label: 'Email',
+                label: 'Gender',
                 field: 'heading2',
                 sort: 'asc',
             },
             {
-                label: 'Phone',
+                label: 'Email',
                 field: 'heading3',
                 sort: 'asc',
             },
             {
-                label: 'Website',
+                label: 'Phone',
                 field: 'heading4',
                 sort: 'asc',
             },
             {
-                label: 'Manage',
+                label: 'Website',
                 field: 'heading5',
                 sort: 'asc',
             },
+            {
+                label: 'Manage',
+                field: 'heading6',
+                sort: 'asc',
+            },
         ],
-        rows: [...userList],
+        rows: gender === 'All' ? [...userList] : [...userFiltered],
     };
 
     return (
@@ -74,8 +98,19 @@ const TablePage = () => {
             <h2 className="text-center mt-5">
                 <u>Striped Table</u>
             </h2>
+            <MDBInput />
+            <div>
+                <select
+                    className="browser-default custom-select border-bottom"
+                    onBlur={handleSelect}
+                >
+                    <option value="All">Filter By Gender: All</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </div>
 
-            <MDBDataTable striped bordered data={data} />
+            <MDBDataTable responsive exportToCSV btn striped bordered data={data} />
             {/* <MDBTable responsive>
                 <MDBTableHead columns={data.columns} />
                 <MDBTableBody rows={data.rows} />
